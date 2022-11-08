c21c2e3be499db40d5d7076b6afd0a7661e131fc
iamtanjil <tanjil4work@gmail.com>
2022-11-09 01:18:03 +0600

    send service data to client complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,14 +12,34 @@ app.use(cors());
 app.use(express.json());
 
 
-//username: getJustice
-//password: aIjlmZjQMOIaSQZY
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Password}@cluster0.jwdgd43.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run () {
+    try{
+        const serviceCollection = client.db('getjustice').collection('services');
+
+        //display 3 services
+        app.get('/limitedservice', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.limit(3).toArray();
+            res.send(services)
+        })
+
+        //display all service data
+        app.get('/allservices', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally{
+
+    }
+}
+run()
 
 
 app.get('/', (req, res) => {
